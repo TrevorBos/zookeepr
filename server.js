0bc6e5be4d9235c252a2cf3f529890e1551fb764
Trevor Bos <trevor.bos@outlook.com>
2022-07-11 09:14:08 -0400

    Fixed a server issue with ports

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Start the server using express
 const express = require("express");
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Route for the front end
@@ -53,6 +54,6 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
-  console.log("API server now on port 3001");
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
